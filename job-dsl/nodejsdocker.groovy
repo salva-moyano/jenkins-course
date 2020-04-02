84abc8b3392fe14ad58e821b36b5c15a73668618
smoyano <39210798+salva-moyano@users.noreply.github.com>
2020-04-02 20:48:36 +0200

    Update with my profile

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/salva-moyano/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('smoyano')
+            node / gitConfigEmail('salvamoyanopalma@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('salvamoyanopalma/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
