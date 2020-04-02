1b89c4c2c1e13947b3f692d9399740f8cec6c3b1
smoyano <39210798+salva-moyano@users.noreply.github.com>
2020-04-02 20:37:04 +0200

    Update Groovy Job

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/salva-moyano/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('smoyano')
+            node / gitConfigEmail('salvamoyanopalma@gmail.com')
         }
     }
     triggers {
